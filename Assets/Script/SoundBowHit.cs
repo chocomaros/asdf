687a1c0e7c616260f3470e406ca7e793cccf26aa
yena <chocomaros@gmail.com>
2016-12-08 21:17:39 +0900

    hit audio

---
+++ Assets/Script/SoundBowHit.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundBowHit : MonoBehaviour {
+
+	public AudioClip Hit;
+	public AudioClip HeadShot;
+	public AudioSource Audio;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider collider){
+		if (collider.tag == "enemy") {
+			Audio.PlayOneShot(Hit);
+		} else if (collider.tag == "enemy_head") {
+			Audio.PlayOneShot (HeadShot);
+		}
+	}
+}
